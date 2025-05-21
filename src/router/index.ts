794b6f57cebe581a799e65726416ce98fe509475
Felix Frank <f.frank@shopware.com>
2025-05-21 17:09:09 +0200

    feat: removed old base 64 test.

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,14 +17,6 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/DiveAR.vue')
     },
-    // {
-    //   path: '/base64',
-    //   name: 'base64',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/DiveBase64.vue')
-    // },
     {
       path: '/material',
       name: 'material',
